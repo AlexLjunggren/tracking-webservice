63b3c74c0c44dafcc03322a12504adbef549fb12
Alex Ljunggren <aljunggren@dminc.com>
2024-07-24 20:04:37 -0400

    Added annotation

--- src/main/java/io/ljunggren/tracking/webservice/model/parcel/FedexParcel.java
+++ src/main/java/io/ljunggren/tracking/webservice/model/parcel/FedexParcel.java
@@ -2,9 +2,10 @@ package io.ljunggren.tracking.webservice.model.parcel;
 
 import java.util.Date;
 
-import io.ljunggren.reportGenerator.annotation.AutoSize;
-import io.ljunggren.reportGenerator.annotation.DateFormatter;
-import io.ljunggren.reportGenerator.annotation.Reportable;
+import io.ljunggren.report.generator.annotation.AutoSize;
+import io.ljunggren.report.generator.annotation.DateFormatter;
+import io.ljunggren.report.generator.annotation.Hyperlink;
+import io.ljunggren.report.generator.annotation.Reportable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -27,6 +28,7 @@ public class FedexParcel extends BaseParcel {
     private Date delivered;
     
     @AutoSize
+    @Hyperlink
     @Reportable(headerName = "Link", column = "G")
     private String link;
     
